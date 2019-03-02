3fa3a110a98c01c9df9b3ba206874efa89bb189b
mateusz <mateusz.basiak@gmail.com>
2019-03-02 20:06:20 +0100

    add claim file

---
+++ src/components/claim.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <h1 class="'claim">Spacer</h1>
+    <p class="subclaim">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </p>
+    <p class="subclaim">
+      type anything spacer ralated to start
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'claim',
+};
+</script>
+
+<style lang="scss" scoped>
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
